aca61fb30cbb5166136bb97bd3e5c9188195b2ab
Austin Shircliff <shircliffa@yahoo.com>
2017-04-19 01:59:08 -0400

    Add files via upload

---
+++ 199 - Program1/Program1/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Program1
+{
+    partial class Program1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.squarefeettext = new System.Windows.Forms.TextBox();
+            this.priceofgallontext = new System.Windows.Forms.TextBox();
+            this.paintcoattext = new System.Windows.Forms.TextBox();
+            this.squarefeet = new System.Windows.Forms.Label();
+            this.coatsofpaint = new System.Windows.Forms.Label();
+            this.priceofgallon = new System.Windows.Forms.Label();
+            this.totalsquarefeet = new System.Windows.Forms.Label();
+            this.totalcost = new System.Windows.Forms.Label();
+            this.costoflabor = new System.Windows.Forms.Label();
+            this.costofpaint = new System.Windows.Forms.Label();
+            this.hoursoflabor = new System.Windows.Forms.Label();
+            this.gallonsofpaint = new System.Windows.Forms.Label();
+            this.totalsquarefeettext = new System.Windows.Forms.Label();
+            this.gallonsofpainttext = new System.Windows.Forms.Label();
+            this.hoursoflabortext = new System.Windows.Forms.Label();
+            this.costofpainttext = new System.Windows.Forms.Label();
+            this.costoflabortext = new System.Windows.Forms.Label();
+            this.totalcosttext = new System.Windows.Forms.Label();
+            this.calculatebutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // squarefeettext
+            // 
+            this.squarefeettext.Location = new System.Drawing.Point(136, 12);
+            this.squarefeettext.Name = "squarefeettext";
+            this.squarefeettext.Size = new System.Drawing.Size(100, 20);
+            this.squarefeettext.TabIndex = 0;
+            // 
+            // priceofgallontext
+            // 
+            this.priceofgallontext.Location = new System.Drawing.Point(136, 69);
+            this.priceofgallontext.Name = "priceofgallontext";
+            this.priceofgallontext.Size = new System.Drawing.Size(100, 20);
+            this.priceofgallontext.TabIndex = 1;
+            // 
+            // paintcoattext
+            // 
+            this.paintcoattext.Location = new System.Drawing.Point(136, 40);
+            this.paintcoattext.Name = "paintcoattext";
+            this.paintcoattext.Size = new System.Drawing.Size(100, 20);
+            this.paintcoattext.TabIndex = 2;
+            // 
+            // squarefeet
+            // 
+            this.squarefeet.AutoSize = true;
+            this.squarefeet.Location = new System.Drawing.Point(65, 15);
+            this.squarefeet.Name = "squarefeet";
+            this.squarefeet.Size = new System.Drawing.Size(65, 13);
+            this.squarefeet.TabIndex = 3;
+            this.squarefeet.Text = "Square Feet";
+            // 
+            // coatsofpaint
+            // 
+            this.coatsofpaint.AutoSize = true;
+            this.coatsofpaint.Location = new System.Drawing.Point(5, 43);
+            this.coatsofpaint.Name = "coatsofpaint";
+            this.coatsofpaint.Size = new System.Drawing.Size(125, 13);
+            this.coatsofpaint.TabIndex = 4;
+            this.coatsofpaint.Text = "Number of Coats of Paint";
+            // 
+            // priceofgallon
+            // 
+            this.priceofgallon.AutoSize = true;
+            this.priceofgallon.Location = new System.Drawing.Point(21, 72);
+            this.priceofgallon.Name = "priceofgallon";
+            this.priceofgallon.Size = new System.Drawing.Size(109, 13);
+            this.priceofgallon.TabIndex = 5;
+            this.priceofgallon.Text = "Price of Paint (Gallon)";
+            // 
+            // totalsquarefeet
+            // 
+            this.totalsquarefeet.AutoSize = true;
+            this.totalsquarefeet.Location = new System.Drawing.Point(38, 111);
+            this.totalsquarefeet.Name = "totalsquarefeet";
+            this.totalsquarefeet.Size = new System.Drawing.Size(92, 13);
+            this.totalsquarefeet.TabIndex = 6;
+            this.totalsquarefeet.Text = "Total Square Feet";
+            // 
+            // totalcost
+            // 
+            this.totalcost.AutoSize = true;
+            this.totalcost.Location = new System.Drawing.Point(75, 226);
+            this.totalcost.Name = "totalcost";
+            this.totalcost.Size = new System.Drawing.Size(55, 13);
+            this.totalcost.TabIndex = 7;
+            this.totalcost.Text = "Total Cost";
+            // 
+            // costoflabor
+            // 
+            this.costoflabor.AutoSize = true;
+            this.costoflabor.Location = new System.Drawing.Point(60, 203);
+            this.costoflabor.Name = "costoflabor";
+            this.costoflabor.Size = new System.Drawing.Size(70, 13);
+            this.costoflabor.TabIndex = 8;
+            this.costoflabor.Text = "Cost of Labor";
+            // 
+            // costofpaint
+            // 
+            this.costofpaint.AutoSize = true;
+            this.costofpaint.Location = new System.Drawing.Point(63, 180);
+            this.costofpaint.Name = "costofpaint";
+            this.costofpaint.Size = new System.Drawing.Size(67, 13);
+            this.costofpaint.TabIndex = 9;
+            this.costofpaint.Text = "Cost of Paint";
+            // 
+            // hoursoflabor
+            // 
+            this.hoursoflabor.AutoSize = true;
+            this.hoursoflabor.Location = new System.Drawing.Point(53, 157);
+            this.hoursoflabor.Name = "hoursoflabor";
+            this.hoursoflabor.Size = new System.Drawing.Size(77, 13);
+            this.hoursoflabor.TabIndex = 10;
+            this.hoursoflabor.Text = "Hours of Labor";
+            // 
+            // gallonsofpaint
+            // 
+            this.gallonsofpaint.AutoSize = true;
+            this.gallonsofpaint.Location = new System.Drawing.Point(49, 134);
+            this.gallonsofpaint.Name = "gallonsofpaint";
+            this.gallonsofpaint.Size = new System.Drawing.Size(81, 13);
+            this.gallonsofpaint.TabIndex = 11;
+            this.gallonsofpaint.Text = "Gallons of Paint";
+            // 
+            // totalsquarefeettext
+            // 
+            this.totalsquarefeettext.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalsquarefeettext.Location = new System.Drawing.Point(136, 101);
+            this.totalsquarefeettext.Name = "totalsquarefeettext";
+            this.totalsquarefeettext.Size = new System.Drawing.Size(100, 23);
+            this.totalsquarefeettext.TabIndex = 17;
+            // 
+            // gallonsofpainttext
+            // 
+            this.gallonsofpainttext.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gallonsofpainttext.Location = new System.Drawing.Point(136, 124);
+            this.gallonsofpainttext.Name = "gallonsofpainttext";
+            this.gallonsofpainttext.Size = new System.Drawing.Size(100, 23);
+            this.gallonsofpainttext.TabIndex = 18;
+            // 
+            // hoursoflabortext
+            // 
+            this.hoursoflabortext.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.hoursoflabortext.Location = new System.Drawing.Point(136, 147);
+            this.hoursoflabortext.Name = "hoursoflabortext";
+            this.hoursoflabortext.Size = new System.Drawing.Size(100, 23);
+            this.hoursoflabortext.TabIndex = 19;
+            // 
+            // costofpainttext
+            // 
+            this.costofpainttext.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.costofpainttext.Location = new System.Drawing.Point(136, 170);
+            this.costofpainttext.Name = "costofpainttext";
+            this.costofpainttext.Size = new System.Drawing.Size(100, 23);
+            this.costofpainttext.TabIndex = 20;
+            // 
+            // costoflabortext
+            // 
+            this.costoflabortext.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.costoflabortext.Location = new System.Drawing.Point(136, 193);
+            this.costoflabortext.Name = "costoflabortext";
+            this.costoflabortext.Size = new System.Drawing.Size(100, 23);
+            this.costoflabortext.TabIndex = 21;
+            // 
+            // totalcosttext
+            // 
+            this.totalcosttext.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalcosttext.Location = new System.Drawing.Point(136, 216);
+            this.totalcosttext.Name = "totalcosttext";
+            this.totalcosttext.Size = new System.Drawing.Size(100, 23);
+            this.totalcosttext.TabIndex = 22;
+            // 
+            // calculatebutton
+            // 
+            this.calculatebutton.Location = new System.Drawing.Point(68, 259);
+            this.calculatebutton.Name = "calculatebutton";
+            this.calculatebutton.Size = new System.Drawing.Size(113, 41);
+            this.calculatebutton.TabIndex = 23;
+            this.calculatebutton.Text = "Calculate";
+            this.calculatebutton.UseVisualStyleBackColor = true;
+            this.calculatebutton.Click += new System.EventHandler(this.calculatebutton_Click);
+            // 
+            // Program1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 312);
+            this.Controls.Add(this.calculatebutton);
+            this.Controls.Add(this.totalcosttext);
+            this.Controls.Add(this.costoflabortext);
+            this.Controls.Add(this.costofpainttext);
+            this.Controls.Add(this.hoursoflabortext);
+            this.Controls.Add(this.gallonsofpainttext);
+            this.Controls.Add(this.totalsquarefeettext);
+            this.Controls.Add(this.gallonsofpaint);
+            this.Controls.Add(this.hoursoflabor);
+            this.Controls.Add(this.costofpaint);
+            this.Controls.Add(this.costoflabor);
+            this.Controls.Add(this.totalcost);
+            this.Controls.Add(this.totalsquarefeet);
+            this.Controls.Add(this.priceofgallon);
+            this.Controls.Add(this.coatsofpaint);
+            this.Controls.Add(this.squarefeet);
+            this.Controls.Add(this.paintcoattext);
+            this.Controls.Add(this.priceofgallontext);
+            this.Controls.Add(this.squarefeettext);
+            this.Name = "Program1";
+            this.Text = "Program 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox squarefeettext;
+        private System.Windows.Forms.TextBox priceofgallontext;
+        private System.Windows.Forms.TextBox paintcoattext;
+        private System.Windows.Forms.Label squarefeet;
+        private System.Windows.Forms.Label coatsofpaint;
+        private System.Windows.Forms.Label priceofgallon;
+        private System.Windows.Forms.Label totalsquarefeet;
+        private System.Windows.Forms.Label totalcost;
+        private System.Windows.Forms.Label costoflabor;
+        private System.Windows.Forms.Label costofpaint;
+        private System.Windows.Forms.Label hoursoflabor;
+        private System.Windows.Forms.Label gallonsofpaint;
+        private System.Windows.Forms.Label totalsquarefeettext;
+        private System.Windows.Forms.Label gallonsofpainttext;
+        private System.Windows.Forms.Label hoursoflabortext;
+        private System.Windows.Forms.Label costofpainttext;
+        private System.Windows.Forms.Label costoflabortext;
+        private System.Windows.Forms.Label totalcosttext;
+        private System.Windows.Forms.Button calculatebutton;
+    }
+}
+
---
+++ 199 - Program1/Program1/Form1.cs
@@ -0,0 +1,86 @@
+﻿//Austin Shircliff
+//Program 1
+//CIS 199-01
+//February 16, 2016
+//This is a program that is designed for a painting company. This program allows the user to input the square feet of the area being painted, the coats of paint, and the price of each gallon of paint. 
+//The program then delivers the total square feet being painted, the gallons of paint required, hours of labor, cost of paint, cost of labor, and the total cost of the job at hand.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program1
+{
+    public partial class Program1 : Form
+    {
+        public Program1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculatebutton_Click(object sender, EventArgs e)//setting the button to activate the program when clicked
+        {
+            try
+            {
+
+                const int SquareFeetPerGallon = 325;//a constant for the square feet a gallon of paint will cover
+                const int laborPerGallon = 8;//a constant for the hours it takes for each gallon of paint
+                const double workerWage = 10.50;//a constant for how much the workers make an hour
+
+                double squarefeet;//a variable for the squarefeet entered 
+                int numberofcoats;// a variable for the number of coats being put on 
+                double pricepergallon;// a variale for the price of gallons that will be entered
+                double totalsquarefeet;//a variable for the total square feet being covered
+                double totalgallons;// a variable for the total number of gallons being used
+                double hoursoflabor;// a variable for the hours of labor that will be required
+                double costofpaint;// a variable for the cost of a gallon of paint
+                double costoflabor;// a variable for the total cost of labor
+                double totalcost;// a variable for the cost of paint + the cost of labor
+
+                squarefeet = double.Parse(squarefeettext.Text);//changing squarefeet to a double
+                numberofcoats = int.Parse(paintcoattext.Text);// changing numberofcoats to an int
+                pricepergallon = double.Parse(priceofgallontext.Text);//changing pricepergallon to a double
+
+
+                totalsquarefeet = squarefeet * numberofcoats;//giving totalsquarefeet a number and calculating for it
+                totalsquarefeettext.Text = totalsquarefeet.ToString("n1");//setting the display and specifying to allow one decimal place
+
+                totalgallons = Math.Ceiling(totalsquarefeet / SquareFeetPerGallon);// setting the math to calculate total gallons with math ceiling to round up
+                gallonsofpainttext.Text = totalgallons.ToString("");//setting the display 
+
+                hoursoflabor = (totalsquarefeet / SquareFeetPerGallon) * laborPerGallon;// setting the math to calculate the hours of labor
+                hoursoflabortext.Text = hoursoflabor.ToString("n1");// setting the display and specifying to show one decimal place
+
+                costofpaint = totalgallons * pricepergallon;// setting the math to calculate the cost of paint
+                costofpainttext.Text = costofpaint.ToString("c");// setting the display and specifying to make it currency
+
+                costoflabor = workerWage * hoursoflabor;//setting the math to calculate total cost of labor
+                costoflabortext.Text = costoflabor.ToString("c");// setting the display and specifying to make it currency
+
+                totalcost = costoflabor + costofpaint;// setting the math to calculate the total cost of the job
+                totalcosttext.Text = totalcost.ToString("c");// setting the display and specifying to make it currency
+
+
+
+
+            }
+            catch (Exception) //used to ensure that data entered is correct
+            {
+            }
+
+
+            }
+         
+        }
+    }
+ 
+
+
+
+
