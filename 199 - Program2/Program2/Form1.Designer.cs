31311bfcf49d0ff79575a7b02fadf8b57ed010c2
Austin Shircliff <shircliffa@yahoo.com>
2017-04-19 01:59:45 -0400

    Add files via upload

---
+++ 199 - Program2/Program2/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Program2
+{
+    partial class Program2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.juniorbutton = new System.Windows.Forms.RadioButton();
+            this.seniorbutton = new System.Windows.Forms.RadioButton();
+            this.sophomorebutton = new System.Windows.Forms.RadioButton();
+            this.freshmanbutton = new System.Windows.Forms.RadioButton();
+            this.nameentry = new System.Windows.Forms.TextBox();
+            this.calculatebutton = new System.Windows.Forms.Button();
+            this.directions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // juniorbutton
+            // 
+            this.juniorbutton.AutoSize = true;
+            this.juniorbutton.Checked = true;
+            this.juniorbutton.Location = new System.Drawing.Point(103, 12);
+            this.juniorbutton.Name = "juniorbutton";
+            this.juniorbutton.Size = new System.Drawing.Size(53, 17);
+            this.juniorbutton.TabIndex = 0;
+            this.juniorbutton.TabStop = true;
+            this.juniorbutton.Text = "Junior";
+            this.juniorbutton.UseVisualStyleBackColor = true;
+            // 
+            // seniorbutton
+            // 
+            this.seniorbutton.AutoSize = true;
+            this.seniorbutton.Location = new System.Drawing.Point(103, 57);
+            this.seniorbutton.Name = "seniorbutton";
+            this.seniorbutton.Size = new System.Drawing.Size(55, 17);
+            this.seniorbutton.TabIndex = 1;
+            this.seniorbutton.Text = "Senior";
+            this.seniorbutton.UseVisualStyleBackColor = true;
+            // 
+            // sophomorebutton
+            // 
+            this.sophomorebutton.AutoSize = true;
+            this.sophomorebutton.Location = new System.Drawing.Point(12, 57);
+            this.sophomorebutton.Name = "sophomorebutton";
+            this.sophomorebutton.Size = new System.Drawing.Size(79, 17);
+            this.sophomorebutton.TabIndex = 2;
+            this.sophomorebutton.Text = "Sophomore";
+            this.sophomorebutton.UseVisualStyleBackColor = true;
+            // 
+            // freshmanbutton
+            // 
+            this.freshmanbutton.AutoSize = true;
+            this.freshmanbutton.Location = new System.Drawing.Point(12, 12);
+            this.freshmanbutton.Name = "freshmanbutton";
+            this.freshmanbutton.Size = new System.Drawing.Size(71, 17);
+            this.freshmanbutton.TabIndex = 3;
+            this.freshmanbutton.Text = "Freshman";
+            this.freshmanbutton.UseVisualStyleBackColor = true;
+            // 
+            // nameentry
+            // 
+            this.nameentry.Location = new System.Drawing.Point(31, 127);
+            this.nameentry.Name = "nameentry";
+            this.nameentry.Size = new System.Drawing.Size(100, 20);
+            this.nameentry.TabIndex = 4;
+            // 
+            // calculatebutton
+            // 
+            this.calculatebutton.Location = new System.Drawing.Point(41, 164);
+            this.calculatebutton.Name = "calculatebutton";
+            this.calculatebutton.Size = new System.Drawing.Size(75, 23);
+            this.calculatebutton.TabIndex = 7;
+            this.calculatebutton.Text = "Calculate";
+            this.calculatebutton.UseVisualStyleBackColor = true;
+            this.calculatebutton.Click += new System.EventHandler(this.calculatebutton_Click);
+            // 
+            // directions
+            // 
+            this.directions.AutoSize = true;
+            this.directions.Location = new System.Drawing.Point(38, 99);
+            this.directions.Name = "directions";
+            this.directions.Size = new System.Drawing.Size(86, 13);
+            this.directions.TabIndex = 8;
+            this.directions.Text = "Enter Last Name";
+            // 
+            // Program2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(173, 199);
+            this.Controls.Add(this.directions);
+            this.Controls.Add(this.calculatebutton);
+            this.Controls.Add(this.nameentry);
+            this.Controls.Add(this.freshmanbutton);
+            this.Controls.Add(this.sophomorebutton);
+            this.Controls.Add(this.seniorbutton);
+            this.Controls.Add(this.juniorbutton);
+            this.Name = "Program2";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton juniorbutton;
+        private System.Windows.Forms.RadioButton seniorbutton;
+        private System.Windows.Forms.RadioButton sophomorebutton;
+        private System.Windows.Forms.RadioButton freshmanbutton;
+        private System.Windows.Forms.TextBox nameentry;
+        private System.Windows.Forms.Button calculatebutton;
+        private System.Windows.Forms.Label directions;
+    }
+}
+
---
+++ 199 - Program2/Program2/Form1.cs
@@ -0,0 +1,229 @@
+﻿//Austin Shircliff
+//Program 2
+//March 9th, 2015
+//CIS 199-01
+//This program allow the user to type in their last name and click a radio button for the specific year and it will return the time and date that they will be able to register for the next years classes.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Program2 : Form
+    {
+        public Program2()
+        {
+            InitializeComponent();
+        }
+        private void calculatebutton_Click(object sender, EventArgs e)
+        {
+            
+            
+            string name = nameentry.Text;//allowing the text entered to be set as a string
+            char entry;//declaring entry as a char
+            entry = 'X';//setting x equal to entry
+            entry = name[0];// saying that it will only look at the first letter in the text
+            entry = char.ToUpper(entry);//setting the entry to always be uppercase
+            //bellow is naming and giving a value to each char
+          
+            char Tletter;
+            Tletter = 'T';
+            char Zletter;
+            Zletter = 'Z';
+            char Aletter;
+            Aletter = 'A';
+            char Dletter;
+            Dletter = 'D';
+            char Eletter;
+            Eletter = 'E';
+            char Iletter;
+            Iletter = 'I';
+            char Jletter;
+            Jletter = 'J';
+            char Oletter;
+            Oletter = 'O';
+            char Pletter;
+            Pletter = 'P';
+            char Sletter;
+            Sletter = 'S';
+            char Vletter;
+            Vletter = 'V';
+            char Wletter;
+            Wletter = 'W';
+            char Bletter;
+            Bletter = 'B';
+            char Cletter;
+            Cletter = 'C';
+            char Fletter;
+            Fletter = 'F';
+            char Gletter;
+            Gletter = 'G';
+            char Lletter;
+            Lletter = 'L';
+            char Mletter;
+            Mletter = 'M';
+            char Qletter;
+            Qletter = 'Q';
+            char Rletter;
+            Rletter = 'R';
+            
+           
+                if (char.IsLetter(entry))//ensuring that it is a letter that is entered
+                {
+                    if (seniorbutton.Checked)//if statement for what will happen is senior is checked
+                    {
+                        //Set the entry greater and equal than the specific requirements, same for all the way down
+                        if (entry >= Tletter && entry <= Zletter)
+                        {
+                            MessageBox.Show("8:30 AM - Wednesday, April 1 2015");//give the appropriate time and date in a messagebox, same for all the way down
+                        }
+                        else if (entry >= Aletter && entry <= Dletter)
+                        {
+                            MessageBox.Show("10:00 AM - Wednesday, April 1 2015");
+                        }
+                        else if (entry >= Eletter && entry <= Iletter)
+                        {
+                            MessageBox.Show("11:30 AM - Wednesday, April 1 2015");
+                        }
+                        else if (entry >= Jletter && entry <= Oletter)
+                        {
+                            MessageBox.Show("2:00 PM - Wednesday, April 1 2015");
+                        }
+                        else if (entry >= Pletter && entry <= Sletter)
+                        {
+                            MessageBox.Show("4:00 PN - Wednesday, April 1 2015");
+                        }
+                    }
+
+                    if (juniorbutton.Checked)
+                    {
+
+                        if (entry >= Tletter && entry <= Zletter)//Set the entry greater and equal than the specific requirements, same for all the way down
+                        {
+                            MessageBox.Show("8:30 AM - Thursday, April 2 2015");//give the appropriate time and date in a messagebox, same for all the way down
+                        }
+                        else if (entry >= Aletter && entry <= Dletter)
+                        {
+                            MessageBox.Show("10:00 AM - Thursday, April 2 2015");
+                        }
+                        else if (entry >= Eletter && entry <= Iletter)
+                        {
+                            MessageBox.Show("11:30 AM - Thursday, April 2 2015");
+                        }
+                        else if (entry >= Jletter && entry <= Oletter)
+                        {
+                            MessageBox.Show("2:00 PM - Thursday, April 2 2015");
+                        }
+                        else if (entry >= Pletter && entry <= Sletter)
+                        {
+                            MessageBox.Show("4:00 PN - Thursday, April 2 2015");
+                        }
+                    }
+                    if (sophomorebutton.Checked)
+                    {
+
+                        if (entry >= Tletter && entry <= Vletter)//Set the entry greater and equal than the specific requirements, same for all the way down
+                        {
+                            MessageBox.Show("8:30 AM - Friday, April 3 2015");//give the appropriate time and date in a messagebox, same for all the way down
+                        }
+                        else if (entry >= Wletter && entry <= Zletter)
+                        {
+                            MessageBox.Show("10:00 AM - Friday, April 3  2015");
+                        }
+                        else if (entry >= Aletter && entry <= Bletter)
+                        {
+                            MessageBox.Show("11:30 AM - Friday, April 3 2015");
+                        }
+                        else if (entry >= Cletter && entry <= Dletter)
+                        {
+                            MessageBox.Show("2:00 PM - Friday, April 3 2015");
+                        }
+                        else if (entry >= Eletter && entry <= Fletter)
+                        {
+                            MessageBox.Show("4:00 PM - Friday, April 3 2015");
+                        }
+                        else if (entry >= Gletter && entry <= Iletter)
+                        {
+                            MessageBox.Show("8:30 AM - Monday, April 6 2015");
+                        }
+                        else if (entry >= Jletter && entry <= Lletter)
+                        {
+                            MessageBox.Show("10:00 AM - Monday, April 6 2015");
+                        }
+                        else if (entry >= Mletter && entry <= Oletter)
+                        {
+                            MessageBox.Show("11:30 AM - Monday, April 6 2015");
+                        }
+                        else if (entry >= Pletter && entry <= Qletter)
+                        {
+                            MessageBox.Show("2:00 PM - Monday, April 6 2015");
+                        }
+                        else if (entry >= Rletter && entry <= Sletter)
+                        {
+                            MessageBox.Show("4:00 PM - Monday, April 6 2015");
+                        }
+                    }
+
+                    if (freshmanbutton.Checked)
+                    {
+
+                        if (entry >= Tletter && entry <= Vletter)//Set the entry greater and equal than the specific requirements, same for all the way down
+                        {
+                            MessageBox.Show("8:30 AM - Tuesday, April 77 2015");//give the appropriate time and date in a messagebox, same for all the way down
+                        }
+                        else if (entry >= Wletter && entry <= Zletter)
+                        {
+                            MessageBox.Show("10:00 AM - Tuesday, April 7  2015");
+                        }
+                        else if (entry >= Aletter && entry <= Bletter)
+                        {
+                            MessageBox.Show("11:30 AM - Tuesday, April 7 2015");
+                        }
+                        else if (entry >= Cletter && entry <= Dletter)
+                        {
+                            MessageBox.Show("2:00 PM - Tuesday, April 7 2015");
+                        }
+                        else if (entry >= Eletter && entry <= Fletter)
+                        {
+                            MessageBox.Show("4:00 PM - Tuesday, April 7 2015");
+                        }
+                        else if (entry >= Gletter && entry <= Iletter)
+                        {
+                            MessageBox.Show("8:30 AM - Wednesday, April 8 2015");
+                        }
+                        else if (entry >= Jletter && entry <= Lletter)
+                        {
+                            MessageBox.Show("10:00 AM - Wednesday, April 8 2015");
+                        }
+                        else if (entry >= Mletter && entry <= Oletter)
+                        {
+                            MessageBox.Show("11:30 AM - Wednesday, April 8 2015");
+                        }
+                        else if (entry >= Pletter && entry <= Qletter)
+                        {
+                            MessageBox.Show("2:00 PM - Wednesday, April 8 2015");
+                        }
+                        else if (entry >= Rletter && entry <= Sletter)
+                        {
+                            MessageBox.Show("4:00 PM - Wednesday, April 8 2015");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please Enter Your Last Name Or The First Letter Of Your Last Name.");
+                }
+            }
+
+
+        }
+    }
+
